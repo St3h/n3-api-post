8738bad6559281c9b3e6e61955060a5bce691ca3
Sefanny <stefanny.lacerda.ismart@gmail.com>
2019-11-08 19:59:32 -0300

    Atualizações de quarta-feira 06-11

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,18 +1,48 @@
-const contatos = require("../model/contatos");
+const model = require("../model/contatos");
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.model);
+  response.status(200).send(model.agenda);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
-};
+// const getById = (request, response) => {
+//   const id = request.params.id;
+//   response.status(200).send(model.find(tarefa => tarefa.id == id));
+// };
+
+// const add = (request, response, contato) => {
+//   let dadosContato = request.body
+//     dadosContato.id = Math.random().toString(36).substr(-8)
+//       console.log(dadosContato)
+//         model.agenda.contatos.push(dadosContato)
+//           response.status(200).send()
+//   // console.log(request.body)
+// }
+
+const add = (request, response) => {
+  let contato = request.body
+  let baseDados = model.agenda.contatos
+
+  contato.id = Math.random().toString(36).substr(-8)
+// Salvar apenas contatos diferentes. Não queremos salvar contatos repetidos na nossa base de dados;
+  if (baseDados.find(dado => dado.nome === contato.nome)){
+    response.status(400).send("Contato já cadastrado")
+} else {
+  model.agenda.contatos.push(contato)
+  response.status(201).send(contato) //serve para colocar algo dentro do contatos.js
+ };
+  
+} 
+
+
+
+
+
 
 
 module.exports = {
   getAll,
-  getById
+  // getById,
+  add
 }
 
