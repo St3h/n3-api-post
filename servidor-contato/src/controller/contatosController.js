4038d6e3bd0af877f200818bcf04189b1599ce03
Sefanny <stefanny.lacerda.ismart@gmail.com>
2019-11-08 20:06:21 -0300

    Alinhamento de texto

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -10,35 +10,17 @@ const getAll = (request, response) => {
 //   response.status(200).send(model.find(tarefa => tarefa.id == id));
 // };
 
-// const add = (request, response, contato) => {
-//   let dadosContato = request.body
-//     dadosContato.id = Math.random().toString(36).substr(-8)
-//       console.log(dadosContato)
-//         model.agenda.contatos.push(dadosContato)
-//           response.status(200).send()
-//   // console.log(request.body)
-// }
-
-const add = (request, response) => {
-  let contato = request.body
-  let baseDados = model.agenda.contatos
-
-  contato.id = Math.random().toString(36).substr(-8)
-// Salvar apenas contatos diferentes. Não queremos salvar contatos repetidos na nossa base de dados;
-  if (baseDados.find(dado => dado.nome === contato.nome)){
-    response.status(400).send("Contato já cadastrado")
-} else {
-  model.agenda.contatos.push(contato)
-  response.status(201).send(contato) //serve para colocar algo dentro do contatos.js
- };
-  
-} 
 
 
 
-
-
-
+const add = (request, response, contato) => {
+  let dadosContato = request.body
+    dadosContato.id = Math.random().toString(36).substr(-8)
+      console.log(dadosContato)
+        model.agenda.contatos.push(dadosContato)
+          response.status(200).send()
+  // console.log(request.body)
+}
 
 module.exports = {
   getAll,
